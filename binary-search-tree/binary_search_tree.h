3931a5c27d33834f347bff7506de08782e753958
dreambucket13 <carmichr@gmail.com>
2022-12-05 06:33:23 -0500

    removing dead code

--- binary-search-tree/binary_search_tree.h
+++ binary-search-tree/binary_search_tree.h
@@ -34,30 +34,6 @@ using tree_ptr = typename std::unique_ptr<binary_search_tree::binary_tree<T>>;
             return false;
         }   
 
-        void internal_insert(T addedData){
-
-            if (addedData <= data()){
-
-                if (left() == nullptr){
-                    leftPtr = tree_ptr (new binary_tree<T>(addedData));
-                    leftPtr->parent = this;
-                } else {
-                    leftPtr->internal_insert(addedData);
-                }
-
-            } else {
-
-                if (right() == nullptr){
-                    rightPtr = tree_ptr (new binary_tree<T>(addedData));
-                    rightPtr->parent = this;
-                } else {
-                    rightPtr->internal_insert(addedData);
-                }
-
-            }
-
-        }
-
 
     public:
 
@@ -119,9 +95,25 @@ using tree_ptr = typename std::unique_ptr<binary_search_tree::binary_tree<T>>;
 
             ++size;
 
-            internal_insert(addedData);
+            if (addedData <= data()){
 
-            //sortedList = this->sort();
+                if (left() == nullptr){
+                    leftPtr = tree_ptr (new binary_tree<T>(addedData));
+                    leftPtr->parent = this;
+                } else {
+                    leftPtr->insert(addedData);
+                }
+
+            } else {
+
+                if (right() == nullptr){
+                    rightPtr = tree_ptr (new binary_tree<T>(addedData));
+                    rightPtr->parent = this;
+                } else {
+                    rightPtr->insert(addedData);
+                }
+
+            }
 
         }
 
@@ -137,63 +129,26 @@ using tree_ptr = typename std::unique_ptr<binary_search_tree::binary_tree<T>>;
 
         //idea - implement a "next" method given a node.  Each node owning it's own sorted list 
         //seems to be problematic.  
-        binary_tree<T>* next(int currentIndex){
 
-            //keep a list of dead-end nodes and the sorted objects.  
-            //go left unless it is a dead end node, else go right.
-            //a node is dead ended if it is min and does not have non-dead end children.
+        binary_tree<T>* up_tree(){
 
-            int counter = 0;
-            int next = ++currentIndex;
+            if (parent == nullptr){
+                return nullptr;
+            } else if (parent->nodeValue > nodeValue) {
+                return parent;
+            } else {
+                return parent->up_tree();    
+            }
+        }
 
-            binary_tree<T>* root = this;
-            binary_tree<T>* index = root;
+        binary_tree<T>* next(){
 
-            std::vector<binary_tree<T>*> sortedObjects = std::vector<binary_tree<T>*>() ;
-            std::vector<binary_tree<T>*> deadEnds = std::vector<binary_tree<T>*>();
-            std::vector<T>sortedList = std::vector<T>();
-
-            while (!isIn(deadEnds, root)){
-                
-                binary_tree<T>* rawLeft = index->leftPtr.get();
-                binary_tree<T>* rawRight = index->rightPtr.get();
-                bool leftIsDeadEnd = isIn(deadEnds, rawLeft);
-                bool rightIsDeadEnd = isIn(deadEnds, rawRight);
-                bool objectIsSorted = isIn(sortedObjects, index);
-
-                if (leftIsDeadEnd && rightIsDeadEnd){
-                    deadEnds.push_back(index);
-                    index = root;
-                } 
-                
-                if ( (rawLeft == nullptr || leftIsDeadEnd) && !objectIsSorted){
-
-                    sortedObjects.push_back(index);
-                    sortedList.push_back(index->nodeValue);
-
-                    if (counter == next){
-                        return index;
-                    } else {
-                        ++counter;
-                    }
-
-
-                    if (rawRight == nullptr || rightIsDeadEnd){
-                        deadEnds.push_back(index);
-                    }
-
-                    index = root;
-
-                } else if (rawLeft != nullptr && !leftIsDeadEnd){
-                    index = rawLeft;
-                } else if (rawRight != nullptr && !rightIsDeadEnd) {
-                    index = rawRight;
-                } 
-
+            if (rightPtr != nullptr){
+                return rightPtr->min();
+            } else {
+                return up_tree();
             }
 
-            return nullptr;
-            
         }
 
         std::vector<T>& sort(){
--- binary-search-tree/tester.cpp
+++ binary-search-tree/tester.cpp
@@ -12,7 +12,7 @@ int main(){
     //build a test tree
 
     auto test_tree = std::unique_ptr<binary_search_tree::binary_tree<uint32_t>>(new binary_search_tree::binary_tree<uint32_t>(4));
-    //test_tree->insert(2);
+    test_tree->insert(2);
     test_tree->insert(6);
     test_tree->insert(3);
     test_tree->insert(8);
@@ -20,21 +20,13 @@ int main(){
     test_tree->insert(5);
     test_tree->insert(7);
 
-
-
+    test_tree->left()->right()->next();
+    test_tree->right()->right()->next();
 
     // auto test_tree = std::unique_ptr<binary_search_tree::binary_tree<string>>(new binary_search_tree::binary_tree<string>("b"));
     // test_tree->insert("a");
     // test_tree->insert("c");
 
-
-    for (int i = 0; i < 10; i++){
-        if (test_tree->next(i) != nullptr){
-            cout << i+1 << "th element next: " << test_tree->next(i)->data() << endl;
-        } else {
-            cout << i+1 << "th element next: not in tree" << endl;
-        }
-    }
 
     //test_tree->sort();
 
