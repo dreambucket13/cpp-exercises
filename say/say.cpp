1f88b6ebff05bac75b746628226615a004b117b0
dreambucket13 <carmichr@gmail.com>
2022-12-05 21:45:51 -0500

    project setup

--- say/say.cpp
+++ say/say.cpp
@@ -2,4 +2,13 @@
 
 namespace say {
 
+    std::string in_english(unsigned long long int input){
+
+        if (input == 0){
+            return "zero";
+        } 
+
+        return "not zero";
+    }
+    
 }  // namespace say
--- say/say.h
+++ say/say.h
@@ -1,8 +1,12 @@
 #if !defined(SAY_H)
 #define SAY_H
 
+#include <string>
+
 namespace say {
 
+    std::string in_english(unsigned long long int input);
+    
 }  // namespace say
 
 #endif // SAY_H
